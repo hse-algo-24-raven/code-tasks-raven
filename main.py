a0909b8d5b01d1a34595d3c39c4026db5403045d
Aleksey Kushnarev <scanavik2005@gmail.com>
2024-11-17 16:00:56 +0500

    Add solution

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def fibonacci_rec(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    
+    if n <= 2:
+        return 1
+    return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
+    
 
 
 def fibonacci_iter(n: int) -> int:
@@ -18,7 +22,16 @@ def fibonacci_iter(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    n_list = [1, 1]
+    for i in range(2, n):
+        n_list.append(n_list[i - 2] + n_list[i - 1])
+    return n_list[-1]
+
+
+def check(n):
+    if not(isinstance(n, int)) or n < 1:
+        return False
+    return True
 
 
 def fibonacci(n: int) -> int:
@@ -27,20 +40,28 @@ def fibonacci(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    prev = 1
+    current = 1
+    for i in range(2, n):
+        tmp = current
+        current = tmp + prev
+        prev = tmp
+    return current
 
-
+@profile()
 def main():
     n = 35
-    print(f"Вычисление {n} числа Фибоначчи рекурсивно:")
-    print(fibonacci_rec(n))
+    if check(n):
+        print(f"Вычисление {n} числа Фибоначчи рекурсивно:")
+        print(fibonacci_rec(n))
 
-    print(f"\nВычисление {n} числа Фибоначчи итеративно:")
-    print(fibonacci_iter(n))
+        print(f"\nВычисление {n} числа Фибоначчи итеративно:")
+        print(fibonacci_iter(n))
 
-    print(f"\nВычисление {n} числа Фибоначчи итеративно без использования массива:")
-    print(fibonacci_iter(n))
-
+        print(f"\nВычисление {n} числа Фибоначчи итеративно без использования массива:")
+        print(fibonacci(n))
+    else:
+        print("Неверный ввод")
 
 if __name__ == "__main__":
     main()
