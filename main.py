bfbe80fae98fe64aaf1b25380c55a5ba07f8d8a5
7lli8 <bachurina.yulia1337@gmail.com>
2024-11-18 22:51:42 +0500

    Выполнения задания 1

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def fibonacci_rec(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+    if n > 2:
+        fib = fibonacci_rec(n - 2) + fibonacci_rec(n - 1)
+        return fib
+    else:
+        return 1
     pass
 
 
@@ -18,6 +23,14 @@ def fibonacci_iter(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+    if (n < 2):
+        return 1
+    F = [0] * (n + 1)
+    F[1] = 1
+    for i in range(2, n + 1):
+        F[i] = F[i - 1] + F[i - 2]
+
+    return F[n]
     pass
 
 
@@ -27,9 +40,23 @@ def fibonacci(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+    if n < 2:
+        return 1
+    if n == 2:
+        return 1
+    F = 0
+    F1 = 1
+    F2 = 1
+
+    for i in range(2, n):
+        F = F1 + F2
+        F2 = F1
+        F1 = F
+
+    return F
     pass
 
-
+@profile()
 def main():
     n = 35
     print(f"Вычисление {n} числа Фибоначчи рекурсивно:")
