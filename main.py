93781ccfb10bf5697416ad907d2d569fe87d8288
HotTryHard <96868576+HotTryHard@users.noreply.github.com>
2024-11-20 01:39:28 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
-from profilehooks import profile
-
-
 def fibonacci_rec(n: int) -> int:
-    """Возвращает N-е число Фибоначчи. Реализована рекурсивно согласно
-    формуле вычисления последовательности.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
-    """
-    pass
-
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
 
 def fibonacci_iter(n: int) -> int:
-    """Возвращает N-е число Фибоначчи. Реализована итеративно с использованием
-    массива для хранения вычисляемых данных.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
-    """
-    pass
-
+    fibonacci_m = [1, 1]
+    for i in range(2, n):
+        fibonacci_m += [fibonacci_m[i - 1] + fibonacci_m[i - 2]]
+    return fibonacci_m[n - 1]
 
 def fibonacci(n: int) -> int:
-    """Возвращает N-е число Фибоначчи. Реализована итеративно без использования массива.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
-    """
-    pass
+    root_5 = 5 ** 0.5
+    phi = ((1 + root_5) / 2)
+    a = ((phi ** n) - ((-phi) ** -n)) / root_5
+    return round(a)
 
 
 def main():
@@ -39,7 +28,7 @@ def main():
     print(fibonacci_iter(n))
 
     print(f"\nВычисление {n} числа Фибоначчи итеративно без использования массива:")
-    print(fibonacci_iter(n))
+    print(fibonacci(n))
 
 
 if __name__ == "__main__":
