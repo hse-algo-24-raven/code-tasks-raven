dbe9e4284e4b93e729baec14bd4df65d23a7d883
frestoweb <146319780+frestoweb@users.noreply.github.com>
2024-11-20 21:17:27 +0500

    update solution for task 1

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ from profilehooks import profile
 
 
 def fibonacci_rec(n: int) -> int:
-    if n <= 2:
-        return 1
-    return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
     """Возвращает N-е число Фибоначчи. Реализована рекурсивно согласно
     формуле вычисления последовательности.
 
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if n <= 2:
+        return 1
+    return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
 
 
 def fibonacci_iter(n: int) -> int:
+    """Возвращает N-е число Фибоначчи. Реализована итеративно с использованием
+    массива для хранения вычисляемых данных.
+
+    :param n: порядковый номер числа Фибоначчи
+    :return: число Фибоначчи
+    """
     if n <= 2:
         return 1
     mas = [0] * n
@@ -23,16 +28,14 @@ def fibonacci_iter(n: int) -> int:
     for i in range(2, n):
         mas[i] = mas[i - 1] + mas[i - 2]
     return mas[n-1]
-    """Возвращает N-е число Фибоначчи. Реализована итеративно с использованием
-    массива для хранения вычисляемых данных.
+
+
+def fibonacci(n: int) -> int:
+    """Возвращает N-е число Фибоначчи. Реализована итеративно без использования массива.
 
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
-
-
-def fibonacci(n: int) -> int:
     prev = 1
     curr = 1
     for i in range(2, n):
@@ -40,12 +43,6 @@ def fibonacci(n: int) -> int:
         curr = tmp + prev
         prev = tmp
     return curr
-    """Возвращает N-е число Фибоначчи. Реализована итеративно без использования массива.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
-    """
-    pass
 
 
 def main():
