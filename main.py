1f8b8bd49eae3a1c5fc6d5155c7e54f317024c1d
frestoweb <146319780+frestoweb@users.noreply.github.com>
2024-11-26 18:45:37 +0500

    Added solution

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
 import random
-
 import numpy as np
 
 MATRIX = "matrix"
 DET = "determinant"
 
 
-def get_random_matrix_and_det(order):
-    """Генерирует случайную квадратную целочисленную матрицу с заранее
-    известным значением определителя.
+def get_matrix(size):
+    det = random.randint(-1000, 1000)
 
-    :param order: порядок матрицы
-    :raise Exception: если порядок матрицы не является целым числом и порядок
-    меньше 1
-    :return: словарь с ключами matrix, det
-    """
-    pass
+    if isinstance(size, int) and isinstance(det, int) and size <= 0:
+        raise Exception("Некорректный тип данных или размер матрицы")
+    if size == 1:
+        return [[det]], det
+
+    while True:
+        matrix = [[0] * size for i in range(size)]
+        diag_elems = [1] * (size - 1) + [det]
+        random.shuffle(diag_elems)
+        for i in range(size):
+            matrix[i][i] = diag_elems[i]
+
+        for i in range(random.randint(size, size * 3)):
+            row1, row2 = random.sample(range(size), 2)
+            coeff = random.randint(-2, 2)
+            for j in range(size):
+                matrix[row1][j] += coeff * matrix[row2][j]
+
+        for i in range(random.randint(size, size * 3)):
+            column1, column2 = random.sample(range(size), 2)
+            coeff = random.randint(-2, 2)
+            for j in range(size):
+                matrix[j][column1] += coeff * matrix[j][column2]
+
+        zero_count = sum(cell == 0 for row in matrix for cell in row)
+        count_elements = size * size
+
+        if zero_count / count_elements <= 0.1:
+            break
+
+    return {MATRIX: matrix, DET: det}
 
 
 def main():
     n = 10
     print(f"Генерация матрицы порядка {n}")
-    result = get_random_matrix_and_det(n)
+    result = get_matrix(n)
     print("\nОпределитель сгенерированной матрицы равен", result[DET])
     print("\n".join(["\t".join([str(cell) for cell in row]) for row in result[MATRIX]]))
     print(
