b6b22098ccb8ad391de322802dfb2893b981fa4f
RomaVin16 <159806983+RomaVin16@users.noreply.github.com>
2024-11-26 23:57:55 +0500

    Выполнили задание 2

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def validate(matrix: list[list[int]]):
+    if not isinstance(matrix, list):
+        raise Exception("Матрица должна быть типа list.")
+    
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+
+    if num_rows != num_cols:
+        raise Exception()
+   
+    for row in matrix:
+        if not isinstance(row, list) or len(row) == 0:
+            raise Exception("Строка не может быт пустой.")
+        
+        for item in row:
+            if not isinstance(item, int):
+                raise Exception("Элементы матрицы должы быть целыми числами.")
+            
 def calculate_determinant(matrix: list[list[int]]) -> int:
     """Вычисляет определитель целочисленной квадратной матрицы
 
@@ -6,8 +24,31 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
-
+    validate(matrix)
+    
+    order = len(matrix)
+    
+    if order == 1:
+        return matrix[0][0]
+ 
+    if order == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    
+    det = 0  
+    for idx, value in enumerate(matrix[0]):
+        if value == 0:
+            continue
+        
+        minor = [
+            [row[col] for col in range(order) if col != idx]
+            for row in matrix[1:]
+        ]
+        
+        co_factor = (-1) ** idx * calculate_determinant(minor)
+        
+        det += value * co_factor
+    
+    return det
 
 def main():
     matrix = [[1, 2], [3, 4]]
