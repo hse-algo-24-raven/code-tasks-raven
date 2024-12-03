62e063771cb3184d9c97df2961be3b4cc1a904ae
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2024-12-04 00:09:01 +0500

    Добавил задание 4+

---
+++ main.py
@@ -0,0 +1,45 @@
+PROFIT = "profit"
+DISTRIBUTION = "distribution"
+DISTRIBUTIONS = "distributions"
+PARAM_ERR_MSG = (
+    "Таблица прибыли от проектов не является прямоугольной "
+    "матрицей с числовыми значениями"
+)
+NEG_PROFIT_ERR_MSG = "Значение прибыли не может быть отрицательно"
+DECR_PROFIT_ERR_MSG = "Значение прибыли не может убывать с ростом инвестиций"
+
+
+class ProfitValueError(Exception):
+    def __init__(self, message, project_idx, row_idx):
+        self.project_idx = project_idx
+        self.row_idx = row_idx
+        super().__init__(message)
+
+
+def get_invest_distributions(
+    profit_matrix: list[list[int]],
+) -> dict[str:int, str : list[list[int]]]:
+    """Рассчитывает максимально возможную прибыль и распределение инвестиций
+    между несколькими проектами. Инвестиции распределяются кратными частями.
+    :param profit_matrix: Таблица с распределением прибыли от проектов в
+    зависимости от уровня инвестиций. Проекты указаны в столбцах, уровни
+    инвестиций в строках.
+    :raise ValueError: Если таблица прибыли от проектов не является
+    прямоугольной матрицей с числовыми значениями.
+    :raise ProfitValueError: Если значение прибыли отрицательно или убывает
+    с ростом инвестиций.
+    :return: Словарь с ключами:
+    profit - максимально возможная прибыль от инвестиций,
+    distributions - списком со всеми вариантами распределения инвестиций между
+    проектами, обеспечивающими максимальную прибыль.
+    """
+    pass
+
+
+def main():
+    profit_matrix = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+    print(get_invest_distributions(profit_matrix))
+
+
+if __name__ == "__main__":
+    main()
