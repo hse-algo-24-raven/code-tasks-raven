f761450084074f7aa50ff6356b7e37761ccd5cd3
AndreyBerdnikov322 <Andreyberdnikov2005@gmail.com>
2024-12-05 18:59:52 +0500

    Выполнено задание 3 вариант 1

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ N_LESS_THAN_K_ERROR_MSG = "Параметр n меньше чем k"
 """Сообщение об ошибке при значении параметра n меньше чем k"""
 
 
+def generate_strings_end_1(n: int) -> list[str]:
+    """Возвращает строки длиной n, которые заканчиваются на 1"""
+    if n == 1:
+        return ["1"]
+    if n == 2:
+        return ["11", "01"]
+    return [s + "1" for s in (generate_strings_end_1(n - 1) + generate_strings_end_0(n - 1))]
+def generate_strings_end_0(n: int) -> list[str]:
+    """Возвращает строки длиной n, которые заканчиваются на 0"""
+    if n == 1:
+        return ["0"]
+    if n == 2:
+        return ["10"]
+    return [s + "0" for s in generate_strings_end_1(n - 1)]
+
+
 def generate_strings(length: int) -> list[str]:
     """Возвращает строки заданной длины, состоящие из 0 и 1, где никакие
     два нуля не стоят рядом.
@@ -20,7 +36,10 @@ def generate_strings(length: int) -> list[str]:
     числом.
     :return: Список строк.
     """
-    pass
+    if not isinstance(length, int) or length <= 0 or isinstance(length, bool):
+        raise ValueError(STR_LENGTH_ERROR_MSG)
+    return generate_strings_end_1(length) + generate_strings_end_0(length)
+    
 
 
 def binomial_coefficient(n: int, k: int, use_rec=False) -> int:
@@ -32,7 +51,32 @@ def binomial_coefficient(n: int, k: int, use_rec=False) -> int:
     числами или значение параметра n меньше чем k.
     :return: Значение биномиального коэффициента.
     """
-    pass
+    if not isinstance(n, int) or not isinstance(k, int):
+        raise ValueError(NOT_INT_VALUE_TEMPL.format("n" if not isinstance(n, int) else "k"))
+    if k < 0:
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("k"))
+    if n < 0:
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("n"))
+    if k > n:
+        raise ValueError(N_LESS_THAN_K_ERROR_MSG)
+    if use_rec:
+        """рекурсивная реализация"""
+        if k == n or k == 0:
+            return 1
+        return binomial_coefficient(n - 1, k) + binomial_coefficient(n - 1, k - 1)
+    else:
+        """итерационная реализация"""
+        matrix = [[0] * (k + 1) for i in range(0, n + 1)]
+        """заполним первый столбец и главную диагональ единицами. получим своеобразную базу"""
+        for i in range(0, n + 1):
+            matrix[i][0] = 1
+        for i in range(1, k + 1):
+            matrix[i][i] = 1
+        for i in range(1, n + 1):
+            for j in range(1, k + 1):
+                matrix[i][j] = matrix[i - 1][j] + matrix[i - 1][j - 1]
+        return matrix[n][k]
+        
 
 
 def main():
